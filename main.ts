cd51885816a7084eb75f89f7ed94475e2d22d89b
Giga <gigabyte5671@gmail.com>
2025-01-28 07:44:35 +1300

    Refactor to make `parse()` more modular

--- main.ts
+++ main.ts
@@ -4,28 +4,39 @@ class Features {
 	public links = true;
 }
 
+interface Reference {
+	markdown: string;
+}
+
+function transformHeadings(reference: Reference): void {
+	reference.markdown = reference.markdown.replace(/^# ([^\n]+)/gm, '<h1>$1</h1>');
+	reference.markdown = reference.markdown.replace(/^## ([^\n]+)/gm, '<h2>$1</h2>');
+	reference.markdown = reference.markdown.replace(/^### ([^\n]+)/gm, '<h3>$1</h3>');
+}
+
+function transformHorizontalRule(reference: Reference): void {
+	reference.markdown = reference.markdown.replace(/^---$/gm, '<hr>');
+}
+
+function transformLinks(reference: Reference): void {
+	reference.markdown = reference.markdown.replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>');
+}
+
 /**
  * Parse markdown into HTML.
  * @param markdown The markdown string.
  * @param features _(Optional)_ A record of features to enable/disable.
  * @returns The parsed HTML string.
  */
-export function parse (markdown: string, features = new Features()) {
-	let html = markdown;
+export function parse(
+	markdown: string,
+	features: Partial<Features> = new Features(),
+): string {
+	const reference: Reference = { markdown };
 
-	if (features.headings) {
-		html = html.replace(/^# ([^\n]+)/gm, '<h1>$1</h1>');
-		html = html.replace(/^## ([^\n]+)/gm, '<h2>$1</h2>');
-		html = html.replace(/^### ([^\n]+)/gm, '<h3>$1</h3>');
-	}
+	if (features.headings) transformHeadings(reference);
+	if (features.horizontalRule) transformHorizontalRule(reference);
+	if (features.links) transformLinks(reference);
 
-	if (features.horizontalRule) {
-		html = html.replace(/^---$/gm, '<hr>');
-	}
-
-	if (features.links) {
-		html = html.replace(/\[([^\]]+)\]\(([^\)]+)\)/g, '<a href="$2">$1</a>');
-	}
-
-	return html;
-};
+	return reference.markdown;
+}
